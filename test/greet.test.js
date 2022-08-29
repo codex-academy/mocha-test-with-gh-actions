a20f055f79bfabe2d65c6aee0adf835230e7d8d6
avermeulen <andre.vermeulen@gmail.com>
2022-08-29 12:33:24 +0200

    renamed test names

--- test/greet.test.js
+++ test/greet.test.js
@@ -4,13 +4,13 @@ import greet from '../greet.js';
 
 describe("The greet function", function() {
     
-    it("should be able to greet", function() {
+    it("should be able to greet Joe", function() {
         
         assert.equal('Hello, Joe', greet("Joe"));
 
     });
 
-    it("should be able to greet", function() {
+    it("should be able to greet Sihle", function() {
         
         assert.equal('Hello, Sihle', greet("Sihle"));
 
